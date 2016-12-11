843fb351457fd120043e67a6c217595d1003361d
lszlawrence <lszlawrence@gmail.com>
2016-12-10 20:09:47 -0500

    port

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@ import com.heroku.sdk.jdbc.DatabaseUrl;
 import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -17,45 +19,10 @@ public class Main {
   public static void main(String[] args) {
 
     Gson gson = new Gson();
-    //port(Integer.valueOf(System.getenv("PORT")));
+    port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
     get("/hello", (req, res) -> "Hello World");
-    /*
-    get("/", (request, response) -> {
-      Map<String, Object> attributes = new HashMap<>();
-      attributes.put("message", "Hello World!");
-
-      return new ModelAndView(attributes, "index.ftl");
-    }, new FreeMarkerEngine());
-
-    get("/db", (req, res) -> {
-      Connection connection = null;
-      Map<String, Object> attributes = new HashMap<>();
-      try {
-        connection = DatabaseUrl.extract().getConnection();
-
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-        ArrayList<String> output = new ArrayList<String>();
-        while (rs.next()) {
-          output.add( "Read from DB: " + rs.getTimestamp("tick"));
-        }
-
-        attributes.put("results", output);
-        return new ModelAndView(attributes, "db.ftl");
-      } catch (Exception e) {
-        attributes.put("message", "There was an error: " + e);
-        return new ModelAndView(attributes, "error.ftl");
-      } finally {
-        if (connection != null) try{connection.close();} catch(SQLException e){}
-      }
-    }, new FreeMarkerEngine());
-
-    });*/
 
     get("/discover", (request, response) -> {
         Map<String, Object> attribute = new HashMap<>();
@@ -140,7 +107,24 @@ public class Main {
      }, gson::toJson);
 
       //get xml all users
-
+      get("/users",(request, response) -> {
+          try{
+              response.type("text/xml");
+              try{
+                  BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/public/xml/users.xml"));
+                  String line;
+                  StringBuilder xml = new StringBuilder();
+                  while((line = bufferedReader.readLine()) != null){
+                      xml.append(line.trim());
+                  }
+                  return xml.toString();
+              }catch (Exception e){
+                  return e.toString();
+              }
+          }catch(Exception e){
+              return e;
+          }
+      });
 
 
   }
